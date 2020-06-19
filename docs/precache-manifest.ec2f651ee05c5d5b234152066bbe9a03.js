2e38847535dd16fbdf53cd908266bb5a41156b43
Regynald Zambrano <zreyleo@gmail.com>
2020-06-18 22:09:45 -0500

    Listo para produccion

---
+++ docs/precache-manifest.ec2f651ee05c5d5b234152066bbe9a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e4f732af72c5521b1b0903bcdacc69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0150bb2c8d265a6fa267",
+    "url": "/static/css/main.0901f659.chunk.css"
+  },
+  {
+    "revision": "3a8df345c46b0ce9d55a",
+    "url": "/static/js/2.3e22b0e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e22b0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0150bb2c8d265a6fa267",
+    "url": "/static/js/main.62bf3c22.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
